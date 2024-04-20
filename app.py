95629dffd45774ddea36c57a97ce53407c1846d2
dayumsam <dayumsam22@gmail.com>
2024-04-20 21:33:08 +0000

    added env to gitignore and imports to app.py

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 from flask import Flask
+
+
+from langchain.agents import Tool
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_community.vectorstores import FAISS
+from pydantic import BaseModel, Field
+
+# get envirionment 
+os.environ.get("OPENAI_API_KEY")
 
 # pip install langchain langchain_openai pypdf faiss-gpu pydantic==1.10.8
 
